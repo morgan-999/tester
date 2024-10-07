93c1824073f3599a94e599d5c588aae65b9db872
morgan-999 <s5648204@bournemouth.ac.uk>
2024-10-07 16:36:39 +0100

    Add files via upload

---
+++ tester.py
@@ -0,0 +1,9 @@
+greeting_string = "Welcome to Programming unit! This is level 4 unit :)"
+print(greeting_string)
+print("The first character is ", greeting_string[0])
+print("The fourth character is", greeting_string[3])
+print("The first five characters are", greeting_string[0:5])
+print("The last character is", greeting_string[-1])
+print("The last four characters are", greeting_string[-4:])
+
+greeting_string[0] = "X"
